fcf4b8e5a1812fba12a7c58bb1e8d4ee8c738ad0
edersonbadeca <edersonbadeca@gmail.com>
2015-07-13 14:53:43 -0300

    Add new operations.

--- manage.sh
+++ manage.sh
@@ -1,5 +1,5 @@
 #! /bin/sh 
-list() {
+list_repositories() {
   cat /etc/apt/sources.list|grep 'deb'|while read line; do
     if ! echo $line | egrep -q '^ *#'
     then
@@ -8,17 +8,30 @@ list() {
   done
 }
 
-install() {
-  cat /etc/apt/sources.list|grep 'deb'|while read line; do
+install_repositories() {
+  cat repositories.txt |grep 'deb'|while read line; do
     if ! echo $line | egrep -q '^ *#'
     then
         sudo add-apt-repository '$line'
+        sudo apt-get update
     fi
   done
 }
+
+list_packages() {
+  dpkg-query -W -f='${Package}\n'
+}
+
+install_packages() {
+  cat packages.txt|while read line; do
+      echo "Installling $line Package.."
+      sudo apt-get install $line
+  done
+}
+
 if [ $# -eq 0 ]
     then
-        echo "Here is your Options:\nlist \ninstall" 
+        echo "Here is your Options:\nlist_repositories \ninstall_repositories \nlist_packages \nInstall_packages" 
     else
         $1
     fi
